a9000bb651e5477326f00223df54f124d0492655
timoel <jobrecool@gmail.com>
2021-08-27 15:37:28 +0200

    Rainbow :D

---
+++ script.js
@@ -0,0 +1,8 @@
+/*function rainbow_anim(element) {
+    document.getElementById(element).style = 'color:black;';
+    setTimeout(lolfunction, 3000);
+}
+
+function lolfunction() {
+    document.getElementById(element).style = 'color:#ededed';
+}*/
